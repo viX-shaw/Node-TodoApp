f66e558210e35718dbee255818b0e0dbdd71f2d1
viX-shaw <vivekkumarshaw9@gmail.com>
2018-03-19 20:15:22 +0530

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ app.post("/todos",(req,res)=>{       //hhtp post method to colloect user input
   });
 });
 
+app.get("/todos",(req,res)=>{
+  Todo.find({}).then((todos)=>{
+     res.send({todos});
+  },(err)=>{
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000,()=>{
   console.log("Server started on port 3000");
 });
