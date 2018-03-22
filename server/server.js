b8944144fd2ff81fcc10caba42d91f41857396ac
viX-shaw <vivekkumarshaw9@gmail.com>
2018-03-22 22:51:53 +0530

    ADDED patch /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var bodyParser=require("body-parser");
-var {ObjectID} = require("mongodb");
+const express = require("express");
+const bodyParser=require("body-parser");
+const {ObjectID} = require("mongodb");
+const lodash = require("lodash");
 
 var {mongoose} = require("./db/mongoose");   //it will run the connect and export statements
 var {Todo} = require("./models/Todo");
@@ -67,6 +68,33 @@ app.delete("/todos/:id",(req,res)=>{
     res.send({todo});
   }).catch((e)=>{
     console.log(e);
+    res.status(400).send();
+  });
+});
+
+app.patch("/todos/:id",(req,res)=>{
+  var id = req.params.id;
+  var body = lodash.pick(req.body ,["text","completed"]);
+  console.log(body);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(lodash.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set :body},{new : true}).then((todo)=>{
+    if(todo === null){
+      res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e)=>{
     res.status(400).send();
   });
 });
